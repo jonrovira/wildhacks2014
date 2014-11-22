3745e52c30b876a3f878c1811fa27c98bef732ce
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-11-22 14:39:08 -0600

    initializing models, finished Mentor model

---
+++ server/models/Mentor.js
@@ -0,0 +1,22 @@
+// Mentor.js
+// Data model for Mentor
+
+var mongoose = require('mongoose');
+
+var mentorSchema = mongoose.Schema({
+	name: String,
+	email: String,
+	mentee: String,
+	menteeID: String,
+	major: String,
+	favSport: String,
+	school: String,
+	year: String,
+	City: String,
+	State: String,
+	picture: String,
+	description: [String]
+
+});
+
+module.exports = mongoose.model('Mentor', mentorSchema);
