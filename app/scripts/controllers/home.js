d614d24100ace5102a7967154b4d409849e9e969
Nevil George <nevilgeorge2016@u.northwestern.edu>
2014-11-22 14:10:15 -0600

    renamed controller

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,7 +8,7 @@
  * Controller of the wildhacks2014App
  */
 angular.module('wildhacks2014App')
-  .controller('MainCtrl', function ($scope) {
+  .controller('HomeCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
